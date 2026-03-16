3db33808f4aef480cdb842c425144b81257dd534
Vishal Chowdhary G.J <vishalchowdhary555@gmail.com>
2026-03-16 18:59:32 +0530

    Refactor optimize function to include scaler and adjust parameters

--- src/ga_optimizer.py
+++ src/ga_optimizer.py
@@ -3,17 +3,20 @@ import numpy as np
 from deap import base, creator, tools, algorithms
 
 
-def optimize(model):
-
-    creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
-    creator.create("Individual", list, fitness=creator.FitnessMin)
+def optimize(model, scaler, fixed_spindle_speed=12000):
+    # Avoid re-creating DEAP classes if script is run multiple times
+    if not hasattr(creator, "FitnessMin"):
+        creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+    if not hasattr(creator, "Individual"):
+        creator.create("Individual", list, fitness=creator.FitnessMin)
 
     toolbox = base.Toolbox()
 
     toolbox.register("feed", random.uniform, 100, 8000)
     toolbox.register("depth", random.uniform, 1, 10)
 
-    toolbox.register("individual",
+    toolbox.register(
+        "individual",
         tools.initCycle,
         creator.Individual,
         (toolbox.feed, toolbox.depth),
@@ -22,31 +25,51 @@ def optimize(model):
 
     toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
-    def objective(ind):
+    def objective(individual):
+        feed, depth = individual
 
-        feed, depth = ind
+        X_raw = np.array([[fixed_spindle_speed, feed, depth]])
+        X_scaled = scaler.transform(X_raw)
 
-        X = np.array([[12000, feed, depth]])
-        preds = model.predict(X)[0]
+        preds = model.predict(X_scaled)[0]
 
-        cost = sum(preds)
+        # Expected prediction order:
+        # cutting_force, surface_roughness, tool_wear_rate,
+        # temperature, chip_load_deviation, power
+        cutting_force = preds[0]
+        surface_roughness = preds[1]
+        tool_wear = preds[2]
+        temperature = preds[3]
+        chip_load_dev = preds[4]
+        power = preds[5]
+
+        # Weighted objective
+        cost = (
+            0.25 * cutting_force +
+            0.25 * surface_roughness +
+            0.20 * tool_wear +
+            0.15 * temperature +
+            0.10 * chip_load_dev +
+            0.05 * power
+        )
 
         return (cost,)
 
     toolbox.register("evaluate", objective)
-
     toolbox.register("mate", tools.cxBlend, alpha=0.5)
-    toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=1, indpb=0.2)
+    toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=200, indpb=0.2)
     toolbox.register("select", tools.selTournament, tournsize=3)
 
-    pop = toolbox.population(n=30)
+    population = toolbox.population(n=30)
 
-    algorithms.eaSimple(pop, toolbox,
-                        cxpb=0.5,
-                        mutpb=0.2,
-                        ngen=20,
-                        verbose=False)
+    algorithms.eaSimple(
+        population,
+        toolbox,
+        cxpb=0.5,
+        mutpb=0.2,
+        ngen=25,
+        verbose=False
+    )
 
-    best = tools.selBest(pop, 1)[0]
-
+    best = tools.selBest(population, 1)[0]
     return best
